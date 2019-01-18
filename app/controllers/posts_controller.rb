bf4f29f5340c2e3b9a5cfc2569823b2c1b4e2c0a
Grace Shih <vocalise@gmail.com>
2019-01-18 05:54:48 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    #@post = Post.find(params[:id])
+    @post = Post.find(params[:id])
     @post.assign_attributes(post_params)
     if @post.valid?
       @post.save
